1d2b4402576b8e640b07ea3028be279ab8e20d9e
Diego dos Santos <diegodossantos.es@gmail.com>
2019-05-25 02:00:15 +0200

    Github repositories list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 
 export default function App() {
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: 'repo-1'},
-    { id: 2, name: 'repo-2'},
-    { id: 3, name: 'repo-3'},
-  ]);
+  const [repositories, setRepositories] = useState([]);
 
-  function handleAddRepository(){
-    setRepositories([...repositories,
-                    {id: Math.random(), name: "nuevo-repo"}])
-  }
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/Diego-Dos-Santos/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
   return (
-    <> 
-     <ul>
-        { repositories.map(repo => (
+    <ul>
+      {repositories.map(repo => (
         <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleAddRepository}>
-        Añadir repositorio
-      </button>
-    </> 
+      ))}
+    </ul>
   );
 }
 
