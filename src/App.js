7788b3e4c94dbbcb1436b483de2c101fad6cb46c
Diego dos Santos <diegodossantos.es@gmail.com>
2019-05-26 02:31:41 +0200

    git repositories with hooks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, {useState, useEffect} from 'react';
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
+
   useEffect(async () => {
     const response = await fetch('https://api.github.com/users/Diego-Dos-Santos/repos');
     const data = await response.json();
@@ -12,10 +13,28 @@ export default function App() {
     setRepositories(data);
   }, []);
 
+  useEffect (() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Tienes ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite (id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+    });
+
+      setRepositories(newRepositories);
+  }
+
   return (
     <ul>
       {repositories.map(repo => (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+        {repo.name}
+        {repo.favorite && <span>(Favorito) </span>}
+        <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
       ))}
     </ul>
   );
